0f4d109b4a7c3c40185410c41872432e96c08e0a
soner1ozturk <soner.ozturk@ryerson.ca>
2025-10-12 13:17:11 -0400

    background image created

---
+++ script.js
@@ -0,0 +1,9 @@
+let body = document.querySelector('body');
+let imageContainer = document.createElement('div');
+imageContainer.className = 'imageContainer';
+body.appendChild(imageContainer);
+
+let contentContainer = document.createElement('div');
+contentContainer.className = 'contentContainer';
+body.appendChild(contentContainer);
+
