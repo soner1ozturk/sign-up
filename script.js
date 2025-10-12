ee96a444a0316cea986cec58f9b7edd1ea10f498
soner1ozturk <soner.ozturk@ryerson.ca>
2025-10-12 13:42:19 -0400

    image container logo and text inserted

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ let imageContainer = document.createElement('div');
 imageContainer.className = 'imageContainer';
 body.appendChild(imageContainer);
 
+let odinLogoContainer = document.createElement('div');
+odinLogoContainer.className = 'odinLogoContainer';
+imageContainer.appendChild(odinLogoContainer);
+
+let odinLogo = document.createElement('div');
+odinLogo.className = 'odinLogo';
+odinLogoContainer.appendChild(odinLogo);
+
+let odinText = document.createElement('span');
+odinText.textContent = 'ODIN';
+odinText.className = 'odinText';
+odinLogoContainer.appendChild(odinText);
+
+//odinLogoContainer.textContent = 'ODIN';
+
 let contentContainer = document.createElement('div');
 contentContainer.className = 'contentContainer';
 body.appendChild(contentContainer);
